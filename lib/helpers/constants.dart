383986b28c8d2db29689001a7b3d85603dd6d3a0
kingfariz <52910517+kingfariz@users.noreply.github.com>
2023-04-16 17:14:56 +0700

    constants & dio setting

---
+++ lib/helpers/constants.dart
@@ -0,0 +1,26 @@
+class EndPoints {
+  static const String games = "games";
+}
+
+class Params {
+  //Keys
+  static const String apiKey = "02ef6ba5d13444ee86bad607e8bce3f4";
+  //Platforms
+  static const String platformPS5 = "187";
+  static const String platformPC = "4";
+  //Orderings
+  static const String orderingNameASC = "name";
+  static const String orderingReleasedASC = "released";
+  static const String orderingAddedASC = "added";
+  static const String orderingCreatedASC = "created";
+  static const String orderingUpdatedASC = "updated";
+  static const String orderingRatingASC = "rating";
+  static const String orderingMetacriticASC = "metacritic";
+  static const String orderingNamedsc = "-name";
+  static const String orderingReleaseddsc = "-released";
+  static const String orderingAddeddsc = "-added";
+  static const String orderingCreateddsc = "-created";
+  static const String orderingUpdateddsc = "-updated";
+  static const String orderingRatingdsc = "-rating";
+  static const String orderingMetacriticdsc = "-metacritic";
+}
---
+++ lib/services/dio_setting.dart
@@ -0,0 +1,42 @@
+import 'package:dio/dio.dart';
+import '../helpers/constants.dart';
+import '../helpers/functions/system_log.dart';
+
+final Dio dio = Dio(BaseOptions(
+    baseUrl: 'https://api.rawg.io/api/',
+    connectTimeout: const Duration(seconds: 30),
+    receiveTimeout: const Duration(seconds: 30),
+    sendTimeout: const Duration(seconds: 30)));
+
+Future<Response> getConnect({
+  required String endpoint,
+  String page = "1",
+  required String platform,
+  String pageSize = "20",
+  required String ordering,
+}) async {
+  try {
+    return await dio.get(endpoint, queryParameters: {
+      'page': page,
+      "keys": Params.apiKey,
+      "page_size": pageSize,
+      "platforms": platform,
+      "ordering": ordering,
+    });
+  } on DioError catch (e) {
+    systemLog(e.toString());
+    rethrow;
+  }
+}
+
+Future<Response> postConnect(url, data) async {
+  try {
+    dio.options.headers['content-Type'] = 'application/x-www-form-urlencoded';
+    dio.options.connectTimeout = const Duration(seconds: 30);
+    dio.options.receiveTimeout = const Duration(seconds: 30);
+    return await dio.post(url, data: data);
+  } on DioError catch (e) {
+    systemLog(e.toString());
+    rethrow;
+  }
+}
